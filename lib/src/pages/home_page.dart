9b7f2d3b15001d27f22e2be55c00c2ac9f97720b
benjasx <benjasanchez175@gmail.com>
2022-11-20 22:44:04 -0700

    se imprimen datos en main

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
+import 'package:login/data%20Get/get_user_name.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,31 +13,76 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+  //document IDs
+  List<String> docIDs = [];
+  Future getDocIDs() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((document) {
+              print(document.reference);
+              docIDs.add(document.reference.id);
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(user.email!,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Color.fromARGB(255, 247, 247, 247))),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.logout,
+                color: Color.fromARGB(255, 235, 16, 0),
+                size: 30,
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hola ${user.email!}',
-                style: const TextStyle(
+            const SizedBox(
+              height: 10,
+            ),
+            const Text('Usuarios Registrados hasta Hoy',
+                style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                     color: Color.fromARGB(255, 8, 80, 139))),
             const SizedBox(
-              height: 20,
+              height: 10,
             ),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Color.fromARGB(255, 214, 34, 34),
-              child: const Text(
-                'Salir',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: getDocIDs(),
+                    builder: (context, snapshot) {
+                      return ListView.builder(
+                        itemCount: docIDs.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListTile(
+                              title: GetUserName(documentId: docIDs[index]),
+                              tileColor: Color.fromARGB(255, 218, 67, 67),
+                              textColor: Color.fromARGB(255, 241, 241, 241),
+                            ),
+                          );
+                        },
+                      );
+                    })),
           ],
         ),
       ),
